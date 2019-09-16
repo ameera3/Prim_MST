08eaf25017f05a847c7d4cd22d4646c9c9297218
Ameera Chowdhury <anchowdh@ucsd.edu>
2019-09-16 12:42:03 -0700

    comments

--- Prim.cpp
+++ Prim.cpp
@@ -1,5 +1,15 @@
 /* Filename: Prim.cpp
- * Description:
+ * Description: The command line program, Prim.cpp, takes in one parameter,
+ *              an input file name. The input file specified by the input
+ *              file name should describe an undirected graph with integer
+ *              edge costs in the following format:
+ *              [number of nodes][number of edges]
+ *              [one node of edge 1][other node of edge 1][weight of edge 1]
+ *              .
+ *              .
+ *              .
+ *              The program will run Prim's algorithm on the given graph
+ *              and output the cost of a minimum spanning tree.
  */
 
 #include <iostream>
